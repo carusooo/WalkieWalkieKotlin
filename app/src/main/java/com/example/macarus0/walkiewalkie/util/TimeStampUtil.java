456b361193ad58507eb95d7d0a9c9090b1f96f7a
Andy Caruso <macaruso@gmail.com>
2018-12-04 20:30:14 -0800

    Improved the handling of timestamps to consider Locales

--- app/src/main/java/com/example/macarus0/walkiewalkie/util/TimeStampUtil.java
+++ app/src/main/java/com/example/macarus0/walkiewalkie/util/TimeStampUtil.java
@@ -5,28 +5,29 @@ import android.util.Log;
 
 import com.example.macarus0.walkiewalkie.R;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import static java.text.DateFormat.getDateInstance;
+
 public class TimeStampUtil {
     private static String TAG = "TimeStampUtil";
     private static String timestampFormatString = "yyyy-MM-dd'T'HH:mm:ssZ";
-    private static String dateFormatString = "dd MMM yyyy";
 
     public static String getStringTimestamp() {
-        SimpleDateFormat sdf = new SimpleDateFormat(timestampFormatString);
-        return sdf.format(new Date());
+        // Returns a
+        return new Date().toString();
     }
 
     public static String getStringDate(String timestamp) {
-        SimpleDateFormat sdf = new SimpleDateFormat(timestampFormatString);
+        SimpleDateFormat sdf = new SimpleDateFormat(timestampFormatString, Locale.US);
         String dateString;
         try {
             Date d = sdf.parse(timestamp);
-            sdf.applyPattern(dateFormatString);
-            dateString = sdf.format(d);
+            dateString = getDateInstance().format(d);
         } catch (ParseException p) {
             Log.e(TAG, "getStringDate: Unable to parse date " + timestamp);
             dateString = timestamp;
@@ -37,20 +38,6 @@ public class TimeStampUtil {
     public static long getTime() {
         Date date = new Date();
         return date.getTime();
-    }
-
-    public static long calculateDuration(String startTimeStamp, String endTimeStamp) {
-        SimpleDateFormat sdf = new SimpleDateFormat(timestampFormatString);
-        long timeDelta;
-        try {
-            Date start = sdf.parse(startTimeStamp);
-            Date end = sdf.parse(endTimeStamp);
-            timeDelta = end.getTime() - start.getTime();
-        } catch (ParseException p) {
-            Log.e(TAG, "calculateDuration: Unable to parse date " + startTimeStamp);
-            timeDelta = 0;
-        }
-        return timeDelta;
     }
 
     public static String getDurationString(Context context, long timeDelta) {
